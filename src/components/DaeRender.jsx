fab62b41158cff065ca2865b259331e3406ed98e
DayOneAI <bambathiam88@gmail.com>
2024-01-03 01:45:47 +0100

    feat: ✨Authentification - Routes

--- src/components/DaeRender.jsx
+++ src/components/DaeRender.jsx
@@ -1,5 +1,7 @@
+import { getAuth, signOut } from 'firebase/auth'
 import { Loader2 } from 'lucide-react'
 import useFetchDAE from '../hooks/useFetchDAE'
+import { Button } from './ui/button'
 
 //Mini component to display loader
 const Loader = () => {
@@ -44,10 +46,19 @@ const DAEList = ({ data }) => {
   )
 }
 const DaeRender = () => {
+  const auth = getAuth()
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   const { data, isLoading, error } = useFetchDAE()
 
   return (
     <>
+      <Button onClick={() => handleSignOut()}>Log out</Button>
       {isLoading && <Loader />}
       {!isLoading && !error && <DAEList data={data} />}
       {error && <ErrorMessage message={error} />}
