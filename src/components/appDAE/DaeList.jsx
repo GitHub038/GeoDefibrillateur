75791129b570c82b03a9401cd43274c07199d922
DayOneAI <bambathiam88@gmail.com>
2024-01-20 01:47:18 +0100

    feat: ✨Setup routes DAE_APP (#37)

---
+++ src/components/appDAE/DaeList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DaeList = () => {
+  return <div>DaeList</div>
+}
+
+export default DaeList
---
+++ src/components/appDAE/RegionList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const RegionList = () => {
+  return <div>RegionList</div>
+}
+
+export default RegionList
