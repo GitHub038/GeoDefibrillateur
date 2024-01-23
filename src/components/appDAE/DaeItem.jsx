02c1b5285c8a874caab938113ededbddc075f3d9
DayOneAI <bambathiam88@gmail.com>
2024-01-24 00:18:25 +0100

    feat: ✨Setup react_Leaflet (#13)

--- src/components/appDAE/DaeItem.jsx
+++ src/components/appDAE/DaeItem.jsx
@@ -9,7 +9,7 @@ const DaeItem = ({ dae }) => {
         className="m-auto flex items-center bg-[#42484d] rounded-lg p-4 pl-[calc(2rem+5px)] cursor-pointer text-inherit no-underline border-l-[5px] border-l-[#00c46a]"
         to={`${gid}?lat=${c_lat_coor1}&lng=${c_long_coor1}`}
       >
-        <span className="">Code Postale : {c_com_cp}</span>
+        <span className="">Code Postal : {c_com_cp}</span>
         {/* <div className="">{c_lat_coor1}</div> */}
 
         {/* <button className="">&times;</button> */}
